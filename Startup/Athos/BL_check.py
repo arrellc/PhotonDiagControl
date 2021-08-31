16d9fecde0c0195c1ffd41a86cf71f2ec2bb941d
Christopher <christopher.arrell@psi.ch>
2021-08-31 15:44:41 +0200

    added beamline files

---
+++ Startup/Athos/BL_check.py
@@ -0,0 +1,46 @@
+from Startuptools import *
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-dir", "--dirName", type=str)
+parser.add_argument("-n", "--NumShots", type=int)
+parser.add_argument("-d", "--ShortDeviceName", type=int)
+
+args = parser.parse_args()
+
+ShortDeviceName = str(args.ShortDeviceName)
+NumShots = args.NumShots
+dirName = args.dirName
+
+print('Device selected: '+ ShortDeviceName)
+
+
+
+# Get device parameters
+
+Device_params = device_param(ShortDeviceName)
+print(Device_params)
+
+# Set file names
+
+fN = dirName +'/'+Device_params['FullName']+'.h5'
+figN =dirName +'/'+Device_params['FullName']+'.h5'
+# Move screen in
+screen_select(Device_params,1)
+
+# Take images
+
+DataOut = get_image(Device_params,NumShots)
+
+# Display images
+
+display_images(DataOut,figN[:-3])
+
+# Save data
+
+save_data(fN, DataOut)
+
+# Move Screen out
+
+screen_select(Device_params,0)
